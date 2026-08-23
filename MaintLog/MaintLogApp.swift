bc721aa9e27473161b2b14945a1b80dd3b6d286c
dfyqtxv <dfyqtxv2y7@privaterelay.appleid.com>
2026-08-23 17:19:11 +0200

    Initial Commit

---
+++ MaintLog/MaintLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaintLogApp.swift
+//  MaintLog
+//
+//  Created by Maciek Witanowski on 23/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct MaintLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MaintLogTests/MaintLogTests.swift
@@ -0,0 +1,19 @@
+//
+//  MaintLogTests.swift
+//  MaintLogTests
+//
+//  Created by Maciek Witanowski on 23/08/2026.
+//
+
+import Testing
+@testable import MaintLog
+
+struct MaintLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}

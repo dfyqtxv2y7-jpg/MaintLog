321f98e8997d2f046e7bb1b8cd116827de53cead
dfyqtxv <dfyqtxv2y7@privaterelay.appleid.com>
2026-08-29 16:57:54 +0200

    [FEAT] new maintlogview + new mainview

---
+++ MaintLog/MainLog/View/MainLogView.swift
@@ -0,0 +1,18 @@
+//
+//  MainLogView.swift
+//  MaintLog
+//
+//  Created by Maciek Witanowski on 29/08/2026.
+//
+
+import SwiftUI
+
+struct MainLogView: View {
+    var body: some View {
+        Text("MaintLOgView")
+    }
+}
+
+#Preview {
+    MainLogView()
+}
--- MaintLog/MainView/View/HomeView.swift
+++ MaintLog/MainView/View/HomeView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text("HOME VIEW")
+        List {
+        
+        }
+        NavigationLink()
     }
 }
 

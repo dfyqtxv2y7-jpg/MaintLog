8dce997755d1a1a4d756ee5b1d3c738971f64e06
dfyqtxv <dfyqtxv2y7@privaterelay.appleid.com>
2026-08-29 13:17:41 +0200

    [FEAT] NEW Home screen  + TabView

---
+++ MaintLog/View/HomeTabView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTabView.swift
+//  MaintLog
+//
+//  Created by Maciek Witanowski on 29/08/2026.
+//
+
+import SwiftUI
+
+struct HomeTabView: View {
+    var body: some View {
+        TabView{
+            Tab("HOMe", systemImage: "house"){
+                NavigationStack(){
+                    HomeView()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeTabView()
+}
---
+++ MaintLog/View/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  MaintLog
+//
+//  Created by Maciek Witanowski on 29/08/2026.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("HOME VIEW")
+    }
+}
+
+#Preview {
+    HomeView()
+}

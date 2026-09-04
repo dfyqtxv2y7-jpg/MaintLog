95a46c449b6dbca8114c00697d4a90ede6ee4b8e
dfyqtxv <dfyqtxv2y7@privaterelay.appleid.com>
2026-09-04 21:24:30 +0200

    [FEAT] create func creaetID

--- MaintLog/CreateMaintLog/View Model/MaintLogFormVM.swift
+++ MaintLog/CreateMaintLog/View Model/MaintLogFormVM.swift
@@ -6,4 +6,24 @@
 //
 
 import Foundation
+import SwiftData
+import Combine
 
+@MainActor final class MaintLogFormVM: ObservableObject {
+    
+    @Published var saveError: String?
+    
+    private func createMaintLogID(arrivalPreviousAirport: String) -> (id: UUID, number: String){
+        
+        let id = UUID()
+        
+        let idPart = id.uuidString.prefix(6).uppercased()
+        let stationPart = arrivalPreviousAirport.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        return(
+            id: id,
+            number: "\(idPart)-\(stationPart)"
+        )
+    }
+    
+}

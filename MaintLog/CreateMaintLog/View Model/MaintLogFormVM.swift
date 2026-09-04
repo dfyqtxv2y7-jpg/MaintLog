96a0d73716d3c45be74b73722ea485c4f6917c05
dfyqtxv <dfyqtxv2y7@privaterelay.appleid.com>
2026-09-04 21:53:16 +0200

    [FEAT] create func save form

--- MaintLog/CreateMaintLog/View Model/MaintLogFormVM.swift
+++ MaintLog/CreateMaintLog/View Model/MaintLogFormVM.swift
@@ -26,4 +26,74 @@ import Combine
         )
     }
     
+    func saveMaintLogForm(
+        in modelContext: ModelContext,
+        departurePreviousAirport: String,
+        departurePreviousTime: Date? = nil,
+        arrivalPreviousAirport: String,
+        arrivalPreviousTime: Date? = nil,
+        flightNumberPreviousFlight: String,
+        departureScheduleAirport: String? = nil,
+        departureScheduleTime: Date? = nil,
+        arrivalScheduleAirport: String? = nil,
+        flightNumberScheduleFlight: String? = nil,
+        aircraftRegistration: String,
+        aircraftType: String,
+        aircraftSubType: String,
+        referenceNo: String,
+        station: String,
+        status: String,
+        hasDefect: Bool? = nil,
+        hasPlannedWork: Bool? = nil,
+        hasOilUplift: Bool? = nil,
+        hasReport: Bool? = nil,
+        customer: String? = nil,
+        createdBy: String? = nil,
+        updatedBy: String? = nil,
+        createdAt: Date? = nil,
+        updatedAt: Date? = nil
+    ) -> MaintLogDataModel? {
+        
+        let identity = createMaintLogID(arrivalPreviousAirport: arrivalPreviousAirport)
+        let now = Date()
+        
+        let log = MaintLogDataModel(
+            id: identity.id,
+            mainLogNumber: identity.number,
+            departurePreviousAirport: departurePreviousAirport,
+            departurePreviousTime: departurePreviousTime,
+            arrivalPreviousAirport: arrivalPreviousAirport,
+            arrivalPreviousTime: arrivalPreviousTime,
+            flightNumberPreviousFlight: flightNumberPreviousFlight,
+            departureScheduleAirport: departureScheduleAirport,
+            departureScheduleTime: departureScheduleTime,
+            arrivalScheduleAirport: arrivalScheduleAirport,
+            flightNumberScheduleFlight: flightNumberScheduleFlight,
+            aircraftRegistration: aircraftRegistration,
+            aircraftType: aircraftType,
+            aircraftSubType: aircraftSubType,
+            referenceNo: referenceNo,
+            station: station,
+            status: status,
+            hasDefect: false,
+            hasPlannedWork: false,
+            hasOilUplift: false,
+            hasReport: false,
+            customer: customer,
+            createdAt: now,
+            updatedAt: now
+            )
+        modelContext.insert(log)
+        
+        do{
+            try modelContext.save()
+            saveError = nil
+            return log
+        } catch {
+            modelContext.rollback()
+            saveError = error.localizedDescription
+            return nil
+            
+        }
+    }
 }
